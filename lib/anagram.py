35b240750df3b8c8d57711be32cd388241cdd0e7
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-05-22 19:35:52 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word= word.lower()
+        self.sorted_word= sorted(self.word)
+        
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if sorted(word.lower()) == self.sorted_word:
+                matches.append(word)
+
+        return matches
+
